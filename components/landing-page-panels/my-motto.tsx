6ab4246ced08b9a6fb222dc57516b23870687c71
aikenwx <59946240+aikenwx@users.noreply.github.com>
2022-11-20 16:59:27 +0800

    Fix build bug

--- components/landing-page-panels/my-motto.tsx
+++ components/landing-page-panels/my-motto.tsx
@@ -8,14 +8,14 @@ export default function MyMotto() {
                     <div className="basis-full">
                         <div className="font-beon text-6xl mb-10">About me</div>
                         <p className="max-w-xl">
-                            I am a software engineer who is passionate about
+                            {`I am a software engineer who is passionate about
                             learning new technology and improving my craft.
                             There's always a more elegant solution and new
                             unexplored potential lying just around the corner,
                             we just need to be curious and persistent enough to
                             find it. I aim to produce high quality software that
                             is scalable and performant, and beautiful, if anyone
-                            is looking.
+                            is looking.`}
                         </p>
                     </div>
 
