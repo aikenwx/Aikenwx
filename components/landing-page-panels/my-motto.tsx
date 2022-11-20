353660124f47e1acdd4e52a7b50e5505bec9a84c
aikenwx <59946240+aikenwx@users.noreply.github.com>
2022-11-20 17:16:05 +0800

    Add flex layout

--- components/landing-page-panels/my-motto.tsx
+++ components/landing-page-panels/my-motto.tsx
@@ -2,24 +2,37 @@ import React from "react";
 
 export default function MyMotto() {
     return (
-        <div className="h-panel bg-teal-600 dark:bg-teal-900 text-white font-light">
-            <div className="xl:container xl:mx-auto">
-                <div className="flex p-10 sm-p-5">
-                    <div className="basis-full">
+        <div className=" bg-teal-600 dark:bg-teal-900 text-white font-light">
+            <div className="xl:container xl:mx-auto h-full flex flex-wrap items-center">
+                <div className="flex p-10 sm-p-5 basis-full">
+                    <div>
                         <div className="font-beon text-6xl mb-10">About me</div>
-                        <p className="max-w-xl">
+                        <p className="max-w-lg">
                             {`I am a software engineer who is passionate about
                             learning new technology and improving my craft.
                             There's always a more elegant solution and new
                             unexplored potential lying just around the corner,
                             we just need to be curious and persistent enough to
                             find it. I aim to produce high quality software that
-                            is scalable and performant, and beautiful, if anyone
+                            is scalable and performant, as well as beautiful, if anyone
                             is looking.`}
                         </p>
                     </div>
-
-                    <div className="basis-full"></div>
+                </div>
+                <div className="flex p-10 sm-p-5 basis-full sm:justify-end ">
+                    <div>
+                        <div className="font-beon text-6xl mb-10">About me</div>
+                        <p className="max-w-lg">
+                            {`I am a software engineer who is passionate about
+                            learning new technology and improving my craft.
+                            There's always a more elegant solution and new
+                            unexplored potential lying just around the corner,
+                            we just need to be curious and persistent enough to
+                            find it. I aim to produce high quality software that
+                            is scalable and performant, as well as beautiful, if anyone
+                            is looking.`}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
